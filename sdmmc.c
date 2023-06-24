8c36c91198584b7bada762cdb68adf2d7d65df4b
Fra <96795244+Alexyo21@users.noreply.github.com>
2023-06-24 04:08:46 +0200

    Add files via upload
    Signed-off-by: Fra <96795244+Alexyo21@users.noreply.github.com>

--- sdmmc.c
+++ sdmmc.c
@@ -276,7 +276,7 @@ int sdmmc_select(void)
 	cmd.c_flags = SCF_RSP_R1B;
 	sdhc_exec_command(card.handle, &cmd);
 	gecko_printf("%s: resp=%x\n", __FUNCTION__, MMC_R1(cmd.c_resp));
- sdhc_dump_regs(card.handle);
+        sdhc_dump_regs(card.handle);
 	
 //	gecko_printf("present state = %x\n", HREAD4(hp, SDHC_PRESENT_STATE));
 	if (cmd.c_error) {
@@ -313,7 +313,7 @@ int sdmmc_read(u32 blk_start, u32 blk_count, void *data)
 {
 	struct sdmmc_command cmd;
 
- gecko_printf("%s(%u, %u, %p)\n", __FUNCTION__, blk_start, blk_count, data);
+        gecko_printf("%s(%u, %u, %p)\n", __FUNCTION__, blk_start, blk_count, data);
 	if (card.inserted == 0) {
 		gecko_printf("sdmmc: READ: no card inserted.\n");
 		return -1;
