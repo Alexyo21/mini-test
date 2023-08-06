f3e74fb9f02a1881f6dc89acd3e2ff5afc1ee709
Fra <96795244+Alexyo21@users.noreply.github.com>
2023-08-06 02:54:30 +0200

    Add files via upload
    Signed-off-by: Fra <96795244+Alexyo21@users.noreply.github.com>

--- starlet.mk
+++ starlet.mk
@@ -6,7 +6,7 @@ PREFIX = $(DEVKITARM)/bin/arm-none-eabi-
 
 CFLAGS = -mbig-endian -mcpu=arm926ej-s -mfloat-abi=soft -mtp=soft -marm -mthumb-interwork
 CFLAGS += -g -ffunction-sections -fno-fast-math
-CFLAGS += -Wall -Wextra -Og -pipe
+CFLAGS += -Wall -Wextra -Os -pipe
 ASFLAGS = -g
 LDFLAGS = -mbig-endian -n -nostartfiles -nodefaultlibs -g -Wl,-gc-sections
 
